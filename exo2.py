a1dd0bfea1dbe0a83a3258c0d6c6912f356ec7ad
RayanMHD <Rayanmahm2019@gmail.com>
2024-12-12 00:18:16 +0100

    exo2 commit update

---
+++ exo2.py
@@ -0,0 +1,9 @@
+Temperature = int(input("Please enter the temperature: "))
+if Temperature<0:
+    print("It's freezing!")
+elif Temperature<10:
+    print("It's cold!")
+elif Temperature<20:
+    print("It's cool!")
+
+print("Stay safe!")
